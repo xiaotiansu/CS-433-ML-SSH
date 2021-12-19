d257ae4e6136d703cf1522a72a93e9a204db76b7
LISH0038 <melodyli710@gmail.com>
2021-12-19 23:29:51 +0100

    Update test_helpers.py

--- Project2/ttt_cifar_improved/utils/test_helpers.py
+++ Project2/ttt_cifar_improved/utils/test_helpers.py
@@ -67,8 +67,8 @@ def load_resnet50(net, head, ssh, classifier, args):
     head_dict["0.weight"] = model_dict["head.fc.weight"]
     head_dict["0.bias"] = model_dict["head.fc.bias"]
     head_dict["2.weight"] = model_dict["head.fc.weight"]
-    head["2.bias"] = model_dict["head.fc.bias"]
-    head_dict.load_state_dict(net_dict)
+    head_dict["2.bias"] = model_dict["head.fc.bias"]
+    net.load_state_dict(net_dict)
     #TODO make it a switch, will need to load to head in the future
     head.load_state_dict(head_dict)
 
