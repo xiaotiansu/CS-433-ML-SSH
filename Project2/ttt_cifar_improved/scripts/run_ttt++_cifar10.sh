096503acbfde2458d9a5a9b491b35a8adfd91b83
LISH0038 <melodyli710@gmail.com>
2021-12-20 11:46:59 +0100

    Update run_ttt++_cifar10.sh

--- Project2/ttt_cifar_improved/scripts/run_ttt++_cifar10.sh
+++ Project2/ttt_cifar_improved/scripts/run_ttt++_cifar10.sh
@@ -26,9 +26,9 @@ fi
 SCALE_EXT=0.05
 SCALE_SSH=0.2
 LR=0.0005
-BS_SSL=16
-BS_ALIGN=16
-QS=16
+BS_SSL=256
+BS_ALIGN=256
+QS=256
 DIVERGENCE=all
 RESUME=save/iwildcam_models/SupCE_iwildcam_resnet50_lr_0.2_decay_0.0001_bsz_256_trial_1
 
